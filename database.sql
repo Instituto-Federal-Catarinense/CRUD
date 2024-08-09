60b23db684c369566da07d850f33563711ef85f0
Gabriel Raupp <gabiraupppimentel@gmail.com>
2024-08-09 11:07:16 -0300

    versão_Raupp

--- database.sql
+++ database.sql
@@ -6,3 +6,11 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user') NOT NULL
 );
+
+
+CREATE TABLE time (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    time number(8) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'user') NOT NULL
+);
---
+++ models/timeModel.js
@@ -0,0 +1,66 @@
+const db = require('../config/db');
+
+const User = {
+    create: (user, callback) => {
+        const query = 'INSERT INTO users (horario, password, role) VALUES (?, ?, ?)';
+        db.query(query, [time.horario, user.password, user.role], (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, results.insertId);
+        });
+    },
+
+    findById: (id, callback) => {
+        const query = 'SELECT * FROM users WHERE id = ?';
+        db.query(query, [id], (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, results[0]);
+        });
+    },
+
+    findByHorario: (horario, callback) => {
+        const query = 'SELECT * FROM users WHERE horario = ?';
+        db.query(query, [horario], (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, results[0]);
+        });
+    },
+
+    update: (id, user, callback) => {
+        const query = 'UPDATE users SET horario = ?, password = ?, role = ? WHERE id = ?';
+        db.query(query, [time.horario, user.password, user.role, id], (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, results);
+        });
+    },
+
+    delete: (id, callback) => {
+        const query = 'DELETE FROM users WHERE id = ?';
+        db.query(query, [id], (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, results);
+        });
+    },
+
+    getAll: (callback) => {
+        const query = 'SELECT * FROM users';
+        db.query(query, (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, results);
+        });
+    },
+};
+
+
+module.exports = User;
