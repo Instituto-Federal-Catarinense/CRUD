0ec52baa78ec5a4f1db9178b24767421d7ddd8d6
Fuyu626 <emanuelaveronezdasilva@gmail.com>
2024-09-06 14:49:20 -0300

    eunaosei

---
+++ crud.sql
@@ -0,0 +1,12 @@
+
+use crud;
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    quantidade INT NOT NULL,
+    categoria INT NOT NULL,
+    FOREIGN KEY (categoria) REFERENCES categorias(id)
+);
+
