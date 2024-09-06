5e6821fac0d64523a389b585e388204bc7762dcf
dani980boo <daniel980boo@gmail.com>
2024-09-06 16:16:14 -0300

    so mudei o database.sql

--- database.sql
+++ database.sql
@@ -2,6 +2,18 @@ CREATE DATABASE CRUD;
 
 USE CRUD;
 
+CREATE TABLE categorias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE vendas (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario INT,
+    produtos INT,
+    total int
+);
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
@@ -9,12 +21,6 @@ CREATE TABLE users (
     role ENUM('admin', 'user') NOT NULL
 );
 
-CREATE TABLE categorias (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL
-);
-
-//crie a tabela produtos com os campos id, nome, descricao e preco
 CREATE TABLE produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
