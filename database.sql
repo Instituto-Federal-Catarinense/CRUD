2e8dc48f78a22d60d254d125d2d7f57cd5d236d7
Paulo <ifcphrw@gmail.com>
2024-09-13 16:05:33 -0300

    13/08

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE CRUD4;
+CREATE DATABASE CRUD;
 
-USE CRUD4;
+USE CRUD;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,10 +8,29 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user') NOT NULL
 );
-CREATE TABLE products (
+
+CREATE TABLE categorias (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    category VARCHAR(255)
+    nome VARCHAR(255) NOT NULL
 );
-//*showdebola
+
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    quantidade INT NOT NULL,
+    categoria INT NOT NULL,
+    FOREIGN KEY (categoria) REFERENCES categorias(id)
+);
+
+CREATE TABLE vendas (
+    id INT PRIMARY KEY,
+    id_user INT,
+    id_produto INT,
+    quantidade INT,
+    valor_total FLOAT,
+    data_venda DATE,
+    FOREIGN KEY (id_user) REFERENCES users(id),
+    FOREIGN KEY (id_produto) REFERENCES produtos(id)
+);
