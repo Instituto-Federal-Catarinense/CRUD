e0eb04041de0a52d323a9697cde6d9400c9cc974
alexmatosz <alexmatos2006@outlook.com>
2024-09-19 20:24:39 -0300

    pegandocommits

--- config/db.js
+++ config/db.js
@@ -14,7 +14,7 @@ connection.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err);
         return;
-    }
+    } 
     console.log('Connected to the MySQL database.');
 });
 
