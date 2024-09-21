646286f5be8c9fbb9c8881f6e72dd65b52540c2d
Bell <133970312+bedobell@users.noreply.github.com>
2024-09-20 23:18:39 -0300

    corrigindo nome dos arquivos

--- database.sql
+++ database.sql
@@ -1,36 +1,42 @@
-CREATE DATABASE CRUD;
+-- Criação do banco de dados
+CREATE DATABASE IF NOT EXISTS CRUD;
 
+-- Uso do banco de dados
 USE CRUD;
 
-CREATE TABLE users (
+-- Tabela para usuários
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user') NOT NULL
 );
 
-CREATE TABLE categorias (
+-- Tabela para categorias
+CREATE TABLE IF NOT EXISTS categorias (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE produtos (
+-- Tabela para produtos
+CREATE TABLE IF NOT EXISTS produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     descricao TEXT NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
     quantidade INT NOT NULL,
     categoria INT NOT NULL,
-    FOREIGN KEY (categoria) REFERENCES categorias(id)
+    FOREIGN KEY (categoria) REFERENCES categorias(id) ON DELETE CASCADE
 );
 
-CREATE TABLE vendas (
+-- Tabela para vendas
+CREATE TABLE IF NOT EXISTS vendas (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    idUser INT NOT NULL,
-    idProduct INT NOT NULL,
+    id_user INT,
+    id_produto INT,
     quantidade INT NOT NULL,
-    valorTotal DECIMAL(10,2) NOT NULL,
-    DataVenda DATE NOT NULL,
-    FOREIGN KEY (idProduct) REFERENCES produtos(id),
-    FOREIGN KEY (idUser) REFERENCES users(id)  
+    valor_total DECIMAL(10,2) NOT NULL,
+    data DATE NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES users(id) ON DELETE SET NULL,
+    FOREIGN KEY (id_produto) REFERENCES produtos(id) ON DELETE CASCADE
 );
