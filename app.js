3d62aa079dc8cd612e8253eefbb04b2c54e05a9c
yuri gabriel <ss.ygabriel2007@gmil.com>
2024-09-21 23:15:22 -0300

    'versaos

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const methodOverride = require('method-override');
 const expressLayouts = require('express-ejs-layouts');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.use(expressLayouts);
+app.use(expressLayouts); // Usando express-ejs-layouts
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
