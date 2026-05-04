06d57def6065266e0ff235b479b7e257f5dc69cb
Gregory Pozzer <pozzergregory@gmail.com>
2026-05-04 11:28:46 -0300

    Crud

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const expressLayouts = require('express-ejs-layouts');
 const indexRoutes = require('./routes/indexRoutes');
 const userRoutes = require('./routes/userRoutes');
 const produtoRoutes = require('./routes/produtoRoutes');
@@ -12,7 +11,6 @@ const PORT = process.env.PORT || 5050;
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-app.use(expressLayouts);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
