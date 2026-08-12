09b1579b068c05f8e89243ac6e522d2381c4ad14
Antonio Cardeal Pereira <cardealpereiraantonio@gmail.com>
2026-08-11 21:59:22 -0300

    refactor(routes): remove unused authentication routes and middleware

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const authController = require('../controllers/authController');
-const { ensureGuest } = require('../middleware/authMiddleware');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,9 +9,5 @@ router.get('/', function(req, res, next) {
     currentUser: req.session ? req.session.user || null : null,
   });
 });
-
-router.get('/login', ensureGuest, authController.renderLoginForm);
-router.post('/login', ensureGuest, authController.login);
-router.post('/logout', authController.logout);
 
 module.exports = router;
